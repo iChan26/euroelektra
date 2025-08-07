19c25f8768d5d622fe4d76435465603814287bcc
iChan26 <penaleschristian45@gmail.com>
2025-08-08 06:55:14 +0800

    Updates

--- pages/sectors_submenu/securityautomation/parkingsystem.js
+++ pages/sectors_submenu/securityautomation/parkingsystem.js
@@ -1,4 +1,5 @@
 "use client";
+import ReloadLink from '../../../components/ReloadLink';
 import { useState, useEffect, useRef, } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
